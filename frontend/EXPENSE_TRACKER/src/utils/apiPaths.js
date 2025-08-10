c8d13405ce5305f2e0b47485c3e777413375ba9c
ARCKBANIGUPTA <arckbanigupta08@gmail.com>
2025-08-10 17:53:36 +0530

    changes in base url

--- frontend/EXPENSE_TRACKER/src/utils/apiPaths.js
+++ frontend/EXPENSE_TRACKER/src/utils/apiPaths.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://expense-tracker-zisr.onrender.com"
+    : "http://localhost:8000";
 
 
 export const API_PATHS = {
